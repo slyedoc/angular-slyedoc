95846ac2d9f638f9fca3fc730135ceb606004263
Patrick Towles <towlesj@nku.edu>
2014-08-15 20:14:02 -0400

    added grunt-release

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,5 +10,10 @@ module.exports = function (grunt) {
         }
     });
     grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-release');
 
+    // Default task(s).
+    grunt.registerTask('default', [
+        'release:patch'
+    ]);
 }
